9222ab34f52f45deb6e636d885ab53a1ba32099b
caveman1234 <443135581@qq.com>
2019-11-17 21:59:30 +0800

    1

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from tkinter import *
-
-from src.components import *
+from tkinter import ttk
+from tkinter import messagebox,filedialog
 
 root = Tk()
 root.geometry("500x500")
+root.title("测试tkinter")
+
+def func(event):
+  file = filedialog.askdirectory(
+    title="打开一个文件。。。。。",
+    # filetypes=[("文本文件","*.txt"),("python文件","*.py")]
+    # initialdir="/Users"
+  )
+  print(file)
+
+label = Label(root,text="i am is label")
+label.bind("<Button-1>",func)
 
 
-
-btn1 = CustomButton(root)
-btn1.pack()
-
-
+label.pack()
 
 root.mainloop()
