e8a184bbd3911a563de0b6ced9f523d827056547
caveman1234 <443135581@qq.com>
2019-11-17 22:47:30 +0800

    d

--- main.py
+++ main.py
@@ -3,21 +3,34 @@ from tkinter import ttk
 from tkinter import messagebox,filedialog
 
 root = Tk()
-root.geometry("500x500")
+root.geometry("1000x1000")
 root.title("测试tkinter")
 
-def func(event):
-  file = filedialog.askdirectory(
-    title="打开一个文件。。。。。",
-    # filetypes=[("文本文件","*.txt"),("python文件","*.py")]
-    # initialdir="/Users"
-  )
-  print(file)
+file1_src = ""
+file2_src = ""
+def func1(event):
+  global file1_src
+  file1_src = filedialog.askopenfilename()
+  label1['text'] = label1['text'] + file1_src
+def func2(event):
+  global file2_src
+  file2_src = filedialog.askopenfilename()
+  label2['text'] = label2['text'] + file2_src
 
-label = Label(root,text="i am is label")
-label.bind("<Button-1>",func)
+btn1 = Button(root,text="请选择需要修改的文件")
+btn2 = Button(root,text="请选择文件二")
+btn3 = Button(root,text="确认转换")
+label1 = Label(root,text="需要修改的文件路径为:")
+label2 = Label(root,text="文件二路径为:")
 
+btn1.bind("<Button-1>",func1)
+btn2.bind("<Button-1>",func2)
 
-label.pack()
+btn1.grid(row=0,column=0,sticky=W)
+btn2.grid(row=0,column=1,sticky=W)
+label1.grid(row=1,column=0,sticky=W)
+label2.grid(row=2,column=0,sticky=W)
+btn3.grid(row=3,column=0,sticky=W)
+
 
 root.mainloop()
