b8b94ac64f841a815b3450b4bd2590258d5ae8cf
caveman1234 <443135581@qq.com>
2019-11-20 22:37:37 +0800

    1

--- src/test/test.py
+++ src/test/test.py
@@ -1,7 +1,13 @@
 
+import face_recognition
+import os
+cwd = os.getcwd()
 
-def func():
-  return "func"
+image_src = os.path.join(cwd,"src/images/p1.png")
+
+image = face_recognition.load_image_file(image_src)
+face_landmarks_list = face_recognition.face_landmarks(image)
+print(face_landmarks_list)
 
 
 
